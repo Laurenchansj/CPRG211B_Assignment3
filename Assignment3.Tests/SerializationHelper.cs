079a0ac50cea9c08cef6aad267a284d241e2f41f
Jiyeon Heo <jiyeon.heo0906@gmail.com>
2023-07-29 12:43:29 -0600

    change parameter for serialization

--- Assignment3.Tests/SerializationHelper.cs
+++ Assignment3.Tests/SerializationHelper.cs
@@ -5,37 +5,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Serialization;
+using Assignment3.Utility; //added
 
 namespace Assignment3.Tests
 {
-    public static class SerializationHelper
-    {
-        /// <summary>
-        /// Serializes (encodes) users
-        /// </summary>
-        /// <param name="users">List of users</param>
-        /// <param name="fileName"></param>
-        public static void SerializeUsers(ILinkedListADT users, string fileName)
-        {
-            DataContractSerializer serializer = new DataContractSerializer(typeof(List<User>));
-            using (FileStream stream = File.Create(fileName))
-            {
-                serializer.WriteObject(stream, users);
-            }
-        }
+	public static class SerializationHelper
+	{
+		/// <summary>
+		/// Serializes (encodes) users
+		/// </summary>
+		/// <param name="users">List of users</param>
+		/// <param name="fileName"></param>
+		public static void SerializeUsers(ILinkedListADT users, string fileName)
+		{
+			//DataContractSerializer serializer = new DataContractSerializer(typeof(List<User>)); change this line to below
+			DataContractSerializer serializer = new DataContractSerializer(typeof(SLL), new Type[] { typeof(Node<User>) });
+			using (FileStream stream = File.Create(fileName))
+			{
+				serializer.WriteObject(stream, users);
+			}
+		}
 
-        /// <summary>
-        /// Deserializes (decodes) users
-        /// </summary>
-        /// <param name="fileName"></param>
-        /// <returns>List of users</returns>
-        public static ILinkedListADT DeserializeUsers(string fileName)
-        {
-            DataContractSerializer serializer = new DataContractSerializer(typeof(List<User>));
-            using (FileStream stream = File.OpenRead(fileName))
-            {
-                return (ILinkedListADT)serializer.ReadObject(stream);
-            }
-        }
-    }
+		/// <summary>
+		/// Deserializes (decodes) users
+		/// </summary>
+		/// <param name="fileName"></param>
+		/// <returns>List of users</returns>
+
+		public static ILinkedListADT DeserializeUsers(string fileName)
+		{
+			//DataContractSerializer serializer = new DataContractSerializer(typeof(List<User>)); change this line to below
+			DataContractSerializer serializer = new DataContractSerializer(typeof(SLL), new Type[] { typeof(Node<User>) });
+			using (FileStream stream = File.OpenRead(fileName))
+			{
+				return (SLL)serializer.ReadObject(stream);
+			}
+		}
+	}
 }
